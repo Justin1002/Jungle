221e40cc7a4b69deb3c2da294bf99c0dabc01a12
Justin Ly <jly3@ualberta.ca>
2021-03-31 00:10:09 +0000

    added category and product count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @product_count = Product.all.count
+    puts @product_count
+    @categories_count = Category.all.count
+    puts @categories_count
   end
 end
