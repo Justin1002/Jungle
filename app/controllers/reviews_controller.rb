6e7ecb007d8d7380bc0389edac644eaaae0d04fd
Justin Ly <jly3@ualberta.ca>
2021-04-07 00:05:44 +0000

    formatted reviews onto table

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,6 +11,13 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def destroy
+    @review = Review.find(params[:id])
+    @product = Product.find(params[:product_id])
+    @review.destroy!
+    redirect_to @product, notice: "Errors"
+  end
+  
     private
 
     def review_params
